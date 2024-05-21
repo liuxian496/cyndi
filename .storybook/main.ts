e6f2a57eaa1e9b39e6caaa528398944e4d7d4c79
liuxian496 <liuxian494@163.com>
2024-05-21 18:40:01 +0800

    style: 修改main.ts 文件的格式化

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -10,27 +10,27 @@ const coverageConfig: AddonOptionsBabel = {
 };
 
 const config: StorybookConfig = {
-  stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
-  addons: [
-    "@storybook/addon-onboarding",
-    "@storybook/addon-links",
-    "@storybook/addon-essentials",
-    "@chromatic-com/storybook",
-    "@storybook/addon-interactions",
-    {
-      name: "@storybook/addon-coverage",
-      options: {
-          ...coverageConfig,
-      },
-  },
-    "@storybook/addon-mdx-gfm"
-  ],
-  framework: {
-    name: "@storybook/react-vite",
-    options: {},
-  },
-  docs: {
-    autodocs: "tag",
-  },
+    stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
+    addons: [
+        "@storybook/addon-onboarding",
+        "@storybook/addon-links",
+        "@storybook/addon-essentials",
+        "@chromatic-com/storybook",
+        "@storybook/addon-interactions",
+        {
+            name: "@storybook/addon-coverage",
+            options: {
+                ...coverageConfig,
+            },
+        },
+        "@storybook/addon-mdx-gfm",
+    ],
+    framework: {
+        name: "@storybook/react-vite",
+        options: {},
+    },
+    docs: {
+        autodocs: "tag",
+    },
 };
 export default config;
